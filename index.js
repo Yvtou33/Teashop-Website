e5932b074b709eadb2c13f1e0eaaea765715882c
Yvtou33 <yvtou@hotmail.fr>
2024-07-31 03:49:33 +0200

    created in a new folder

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let button1 = document.querySelector(".btn1");
+
+let button3 = document.querySelector(".btn3");
+
+button1.onclick = function clickFirstBtn() {
+    location.href = "products.html";
+   // alert("Disponible prochainement")
+}
+
+
+button3.onclick = function clickFirstBtn() {
+    location.href = "products.html";
+}
+
+/*function calculTaxe (prix, tva = 0.1) {//0.1 est la valeur par défaut du paramètre
+    console.log (prix * tva);
+}
+
+calculTaxe (2000, 0.15);
+calculTaxe (5000);*/
+
+
